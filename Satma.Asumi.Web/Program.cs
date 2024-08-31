130baf8bdda95f173ced4617d3e0a3109814b3f1
Bogdan Shahnitsky <satma0745@gmail.com>
2024-08-31 14:11:14 +0300

    Use MudBlazor instead of the Bootstrap.

--- Satma.Asumi.Web/Program.cs
+++ Satma.Asumi.Web/Program.cs
@@ -1,6 +1,9 @@
+using MudBlazor.Services;
 using Satma.Asumi.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
